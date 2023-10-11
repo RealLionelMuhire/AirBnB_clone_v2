b06ecb93b501bb72be7a83ee68a49f94777fc592
RealLionelMuhire <muhirelionel@gmail.com>
2023-10-11 09:13:51 +0000

    adding a pack web static file

---
+++ 1-pack_web_static.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This module contains the finction do_pack that generates  a .tgz from the
+contents of the web_statoc folder (fabric script).
+"""
+
+from fabric.api import local
+from datetime import datetime
+
+def do_pack():
+    # Create the 'versions' directory if it doesn't exist
+    local("mkdir -p versions")
+
+    # Get the current date and time for the archive filename
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+
+    # Define the filename with the full path
+    filename = "versions/web_static_{}.tgz".format(date)
+
+    # Create the .tgz archive from the 'web_static' directory
+    result = local("tar -cvzf {} web_static".format(filename))
+
+    if result.succeeded:
+        return filename
+    else:
+        return None
