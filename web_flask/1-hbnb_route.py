b173c48afe4f77703fd6806e28523fabcbe38f1b
RealLionelMuhire <muhirelionel@gmail.com>
2023-10-21 21:31:00 +0200

    pycode

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
+"""creating some 2 path route"""
+
 from flask import Flask
 
-app =  Flask(__name__)
+
+app = Flask(__name__)
+
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """Hello HBNB!"""
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """HBNB"""
     return 'HBNB'
 
 
