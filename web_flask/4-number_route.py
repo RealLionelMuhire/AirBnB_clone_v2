5c07a9fc536be0f2817700a550e501fa7b9630f0
RealLionelMuhire <muhirelionel@gmail.com>
2023-10-21 23:40:47 +0200

    number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""creating some 2 path route"""
+
+from flask import Flask, escape
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):  # Fix the parameter name here
+    """Replace underscores (_) with spaces"""
+    text = text.replace("_", " ")
+    return 'C ' + text
+
+
+@app.route("/python/", defaults={"text": "is cool"}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text):
+    """Replacing text with passed one"""
+    return "Python " + escape(text.replace("_", " "))
+
+
+@app.route("/number/<int:n>")
+def number(n):
+    """returning a number"""
+    return f"{n} is a number"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
